3bea04ff77eff01bcabb05ef805f1b1d18abc548
Prashant Parashar <prashant112006@gmail.com>
2026-07-28 08:11:29 +0000

    commit

---
+++ 02_basics/array.js
@@ -0,0 +1,10 @@
+const myArray = [1,2,3,4,5]
+//console.log(myArray);
+
+console.log(myArray.push(6))
+const newArr = myArray.join()
+//console.log(typeof(newArr))
+
+console.log(myArray.slice(1,3))
+console.log(myArray.splice(1,3))
+
