84bf2ff40301ae07f8f3672e0c3c648bf9e43967
Prashant Parashar <prashant112006@gmail.com>
2026-07-29 09:19:21 +0000

    iife

---
+++ 03_basics/iife.js
@@ -0,0 +1,16 @@
+
+// (function(){
+//     console.log("hii im prashant parashar")
+// })
+// ()
+
+//iife function using arrow function 
+
+// (()=>{
+//     console.log("hii everyone narendra modi is the primeminister of india")
+// })()
+
+
+((container1,container2)=>{
+    console.log(`first value is ${container1} and second value is ${container2}`)
+})(23,20)
