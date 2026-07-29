4031f1a0db1d8df9658303dea3c9c4ac0c18723c
Prashant Parashar <prashant112006@gmail.com>
2026-07-29 11:25:36 +0000

    loop

---
+++ 05_iteration/loop.js
@@ -0,0 +1,9 @@
+// for(let i=0;i<10;i++){
+//     console.log(`${i}`);
+    
+// }
+const myArray=["prashant","rohit","kohli","sachin"]
+for(let i=0;i<myArray.length;i++){
+    const result=myArray[i]
+    console.log(result)
+}
