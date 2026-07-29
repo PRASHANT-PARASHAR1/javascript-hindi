57c0d2f16733eaf966281c7a38f8c82c8cdac199
Prashant Parashar <prashant112006@gmail.com>
2026-07-29 10:56:22 +0000

    controlflow

--- 04_controlFlow/first.js
+++ 04_controlFlow/first.js
@@ -78,3 +78,22 @@ else{
     console.log("your password is incorrect")
     console.log("plese try again")
 }
+
+
+
+let result = age >= 18 ? "Adult" : "Minor";
+console.log(result);
+
+// 6. Logical Operators
+let hasLicense = true;
+
+if (age >= 18 && hasLicense) {
+  console.log("You can drive");
+}
+
+//terniary operator
+let age = 20;
+
+let result = age >= 18 ? "Adult" : "Minor";
+
+console.log(result);
