63bd2ba8fe6c129098c262d7d67cf0f5bda0207e
Prashant Parashar <prashant112006@gmail.com>
2026-07-29 10:14:42 +0000

    control

---
+++ control_flow/first.js
@@ -0,0 +1,80 @@
+// // JavaScript Conditions - All Examples in One Box
+
+// let age = 20;
+// let day = 2;
+// let marks = 75;
+
+// // 1. if
+// if (age >= 18) {
+//   console.log("Eligible to vote");
+// }
+
+// // 2. if...else
+// if (age >= 18) {
+//   console.log("Adult");
+// } else {
+//   console.log("Minor");
+// }
+
+// // 3. if...else if...else
+// if (marks >= 90) {
+//   console.log("Grade A");
+// } else if (marks >= 75) {
+//   console.log("Grade B");
+// } else if (marks >= 60) {
+//   console.log("Grade C");
+// } else {
+//   console.log("Fail");
+// }
+
+// // 4. switch
+// switch (day) {
+//   case 1:
+//     console.log("Monday");
+//     break;
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+//   default:
+//     console.log("Invalid Day");
+// }
+
+// // 5. Ternary Operator
+// let result = age >= 18 ? "Adult" : "Minor";
+// console.log(result);
+
+// // 6. Logical Operators
+// let hasLicense = true;
+
+// if (age >= 18 && hasLicense) {
+//   console.log("You can drive");
+// }
+
+// if (age < 18 || hasLicense) {
+//   console.log("Condition is true");
+// }
+
+// console.log(!false); // true
+
+
+//***********BANK ATM EXAMPLE */
+
+
+const password=1234;
+if(password){
+console.log("enter your dabit card");
+const currentBalance=10000;
+const money;
+console.log(`enter your money ${money}`)
+if(money<=1000){
+    currentBalance-money
+    console.log(`current balance is ${currentBalance}`)
+    
+}
+else{
+    console.log(`your current balance is ${currentBalance}`)
+}
+}else{
+    console.log("your password is incorrect")
+    console.log("plese try again")
+}
