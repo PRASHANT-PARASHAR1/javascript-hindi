86c2e871cd39f63f9784d58f0e9a6f7ee7000ce9
Prashant Parashar <prashant112006@gmail.com>
2026-08-09 08:46:32 +0000

    commit

--- 10_class_oop/inheritance.js
+++ 10_class_oop/inheritance.js
@@ -3,8 +3,26 @@ class User{
         this.username=username
     }
     logMe(){
-        console.log(`userName is ${this.username}`);6
+        console.log(`userName is ${this.username}`);
         
     }
     
 }
+class Teacher extends User{
+    constructor(username,email,password){
+        super(username)
+        this.email=email
+        this.password=password
+    }
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);
+        
+    }
+}
+const result = new Teacher("prashant","prashant@123","123")
+console.log(result);
+console.log(result.addCourse());
+console.log(result.logMe());
+console.log(User instanceof Teacher);  //false
+
+
