0bbd33550ef2bc7fa56249e4d0cfe5fb07fee6e6
Prashant Parashar <prashant112006@gmail.com>
2026-08-09 04:47:37 +0000

    commit

---
+++ 10_class_oop/prototype.js
@@ -0,0 +1,96 @@
+// function multiply5(num){
+// return num*5
+// }
+
+// console.log(multiply5.power)
+// console.log(multiply5.prototype)
+
+function createUser(username,score){
+    this.username=username
+    this.score=score
+    
+
+}
+createUser.prototype.increment = function(){
+    score++
+    // console.log(`increment is ${this.score}`);
+    
+}
+createUser.prototype.printMe = function(){
+    console.log(`the tea price is ${this.score}`)
+}
+const chai = new createUser("chai",25)
+const tea = createUser("tea",250)
+
+chai.printMe()
+
+
+//***********prototype*********/
+
+// let myName = "hitesh     "
+// let mychannel = "chai     "
+
+// console.log(myName.trueLength);
+
+
+let myHeros = ["thor", "spiderman"]
+
+
+let heroPower = {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+}
+
+Object.prototype.hitesh = function(){
+    console.log(`hitesh is present in all objects`);
+}
+
+Array.prototype.heyHitesh = function(){
+    console.log(`Hitesh says hello`);
+}
+
+// heroPower.hitesh()
+// myHeros.hitesh()
+// myHeros.heyHitesh()
+// heroPower.heyHitesh()
+
+// inheritance
+
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+let anotherUsername = "ChaiAurCode     "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`True length is: ${this.trim().length}`);
+}
+
+anotherUsername.trueLength()
+"hitesh".trueLength()
+"iceTea".trueLength()
